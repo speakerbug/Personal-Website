19ef1a523fc08e9c2f83e3588a853312c9a556bc
Henry Saniuk <henry@henrysaniuk.com>
2025-10-22 20:59:44 -0400

    Adjust calendar page copy and reusable noindex config (#8)

--- app/robots.ts
+++ app/robots.ts
@@ -6,7 +6,7 @@ export default function robots(): MetadataRoute.Robots {
       {
         userAgent: '*',
         allow: '/',
-        disallow: ['/api/', '/_next/'],
+        disallow: ['/api/', '/_next/', '/calendar', '/calendar/'],
       },
     ],
     sitemap: 'https://henrysaniuk.com/sitemap.xml',
